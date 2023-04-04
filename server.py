1b6139d055e19787e011a8cf35d40ccf78435bcc
woo1 <projwi@gmail.com>
2023-04-04 13:13:09 +0900

    메시지 전송부분 수정

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from websocket_server import WebsocketServer
+import json
+
+def get_msg_bytes(cliend_id, msg):
+	return json.dumps({"id": cliend_id, "msg": msg})
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
 	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+	server.send_message_to_all(get_msg_bytes("server", str(client['id']) + " has joined us"))
 
 
 # Called for every client disconnecting
@@ -16,6 +20,7 @@ def message_received(client, server, message):
 	if len(message) > 200:
 		message = message[:200]+'..'
 	print("Client(%d) said: %s" % (client['id'], message))
+	server.send_message_to_all(get_msg_bytes(client['id'], message))
 
 
 PORT=9001
